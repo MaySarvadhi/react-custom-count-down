cd1a41a13e3a76bbfcb8736b6ea275c2207d786c
“maysolanki” <mayur.solanki@iblinfotech.com>
2021-08-04 14:36:52 +0530

    loader issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,31 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./dist/index.ts",
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx|ts|tsx)$/,
-            exclude: /node_modules[/\\](?!react-native-vector-icons)/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ["@babel/preset-env", "@babel/preset-react"],
-              },
-            },
-          },
-          {
-            test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
-            type: "asset",
-          },
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-        ],
+  // bundling mode
+  mode: "production",
+
+  // entry files
+  entry: "./dist/index.ts",
+
+  // output bundles (location)
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+
+  // file resolutions
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+
+  // loaders
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
-}
+    ],
+  },
+};
