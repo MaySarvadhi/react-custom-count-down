6699582e8fb8040c4473774a88f9aca61721b5a3
“maysolanki” <mayur.solanki@iblinfotech.com>
2021-08-04 15:20:00 +0530

    set up web loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,19 @@
 const path = require("path");
 
 module.exports = {
-  // bundling mode
   mode: "production",
-
-  // entry files
   entry: "./dist/index.ts",
 
-  // output bundles (location)
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
   },
 
-  // file resolutions
+
   resolve: {
     extensions: [".ts", ".js", ".tsx"],
   },
 
-  // loaders
   module: {
     rules: [
       {
