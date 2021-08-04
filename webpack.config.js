2ed0061dba61aa1bda1fbe733349c7fb7934513b
“maysolanki” <mayur.solanki@iblinfotech.com>
2021-08-04 14:24:19 +0530

    loader issue

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,31 @@ const path = require("path");
 module.exports = {
     entry: "./dist/index.ts",
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
     },
-    module:{
-        rules:[{
-            loader: 'babel-loader',
-            test: '/\.(js|jsx)$/',
-            exclude: /node_modules/
-        }]
-    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx|ts|tsx)$/,
+            exclude: /node_modules[/\\](?!react-native-vector-icons)/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"],
+              },
+            },
+          },
+          {
+            test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+            type: "asset",
+          },
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public')
